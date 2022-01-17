dc465bc2eab7cc5453f7a763b68e08b9e1058c36
MariDreamer <85796352+MariDreamer@users.noreply.github.com>
2022-01-17 14:41:56 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,7 +1,10 @@
 terraform {
   required_providers {
   
-
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.72.0"
+    }
     random = {
       source  = "hashicorp/random"
       version = "3.0.0"
