43cca3761a25210055fc4bad50b4f83bfc5a005f
MariDreamer <85796352+MariDreamer@users.noreply.github.com>
2022-01-17 14:48:01 +0100

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,15 +1,15 @@
-terraform {
-  required_providers {
-  
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.72.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.0.0"
-    }
-  }
-
-  required_version = ">= 0.14.0"
-}
+#terraform {
+#  required_providers {
+#  
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "3.72.0"
+#    }
+#    random = {
+#      source  = "hashicorp/random"
+#      version = "3.0.0"
+#    }
+#  }
+#
+# required_version = ">= 0.14.0"
+#}
